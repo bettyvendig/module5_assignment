a3b59b7a8cbce2f6901c67c5002b9c844d1cff0b
bettyvendig <46284850+bettyvendig@users.noreply.github.com>
2019-02-06 14:21:52 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -109,7 +109,7 @@ function buildAndShowHomeHTML (categories) {
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
 
-		insertProperty (homeHtml, "randomCategoryShortName", "A");
+		insertProperty (homeHtml, "randomCategoryShortName", "'A'");
 		//ert(shortName);
 		
 		
